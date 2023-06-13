366b9a6d64c349243338d08329c254c9e55396a2
Zy5879 <ZaireM21@gmail.com>
2023-06-13 16:59:42 -0400

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./sections/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <section>
+        <p>Zaire McAllister</p>
+      </section>
+      <section>
+        <p>Projects</p>
+        <p>About</p>
+        <p>Contact</p>
+      </section>
+    </nav>
+  );
+}
